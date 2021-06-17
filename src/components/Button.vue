f8288295106e476194ded9a0b1bc2f4159e15ba8
Karan Odedra <karanodedra81@gmail.com>
2021-06-17 10:59:26 +0530

    initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    @click="$emit('btn-click')"
+    :style="{ background: color }"
+    class="btn"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    color: String,
+  },
+  emits: ['btn-click'],
+  // methods: {
+  //   onClick() {
+  //     console.log('Click');
+  //   },
+  // },
+};
+</script>
